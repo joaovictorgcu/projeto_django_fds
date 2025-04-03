16c83a944281cde1201da70c21f9881eb68c4263
João Victor Uchôa <jvgcu@cesar.school>
2025-04-03 17:45:05 -0300

    adicionado recurso para que o usuário consiga, se quiser, colocar a placa do carro.

--- cars/models.py
+++ cars/models.py
@@ -15,6 +15,7 @@ class Car(models.Model):
     brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
     factory_year = models.IntegerField(blank = True, null = True)
     model_year = models.IntegerField(blank = True, null = True)
+    plate = models.CharField(max_length=10, null=True, blank = True)
     value = models.FloatField(blank = True, null = True)
 
     def __str__(self):
