eaae975dd9dbabf24e098c19070f24fe82940107
João Victor Uchôa <jvgcu@cesar.school>
2025-04-04 09:14:23 -0300

    Adicionando a possibilidade do usuário comum adicionar carros ao site.

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-from cars.models import Car
-
+from django.shortcuts import render, redirect
+from cars.models import Car, Brand
 
 def cars_view(request): 
     cars = Car.objects.all().order_by('model')
@@ -14,3 +13,33 @@ def cars_view(request):
         'cars.html',
         {'cars': cars}
     )
+
+from cars.models import Brand, Car
+from django.shortcuts import render, redirect
+
+def new_car_view(request):
+    if request.method == "POST":
+        model = request.POST.get("model")
+        brand_id = request.POST.get("brand")
+        factory_year = request.POST.get("factory_year")
+        model_year = request.POST.get("model_year")
+        plate = request.POST.get("plate")
+        value = request.POST.get("value")
+        photo = request.FILES.get("photo")
+
+        brand = Brand.objects.get(id=brand_id)
+
+        Car.objects.create(
+            model=model,
+            brand=brand,
+            factory_year=factory_year,
+            model_year=model_year,
+            plate=plate,
+            value=value,
+            photo=photo
+        )
+
+        return redirect("cars_list")
+    
+    brands = Brand.objects.all()
+    return render(request, 'new_car.html', {'brands': brands})
