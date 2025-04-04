95394480e975870b0c1ac381f4cc2439764bb9c2
João Victor Uchôa <jvgcu@cesar.school>
2025-04-03 23:14:03 -0300

    Melhorando o HTML

--- cars/views.py
+++ cars/views.py
@@ -3,7 +3,7 @@ from cars.models import Car
 
 
 def cars_view(request): 
-    cars = Car.objects.all().order_by('-model')
+    cars = Car.objects.all().order_by('model')
     search = request.GET.get('search')
     
     if search:
