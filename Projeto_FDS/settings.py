6e0149ce7b36c491f19b55a9031bb7159994fa86
João Victor Uchôa <jvgcu@cesar.school>
2025-04-03 23:28:07 -0300

    Configurando o base template

--- Projeto_FDS/settings.py
+++ Projeto_FDS/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Projeto_FDS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Projeto_FDS/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
