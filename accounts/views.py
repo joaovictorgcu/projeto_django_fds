629d7fa3a5419d4c55a5eddd61a14ee778d07b02
João Victor Uchôa <jvgcu@cesar.school>
2025-04-04 10:14:46 -0300

    Criando tela de login

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.contrib import messages
+
 
 def register_view(request):
     if request.method == "POST":
@@ -21,3 +23,19 @@ def register_view(request):
         return redirect('login')
 
     return render(request, "register.html")
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login realizado com sucesso!")
+            return redirect('cars_list')  # redireciona para a página de carros, por exemplo
+        else:
+            messages.error(request, "Usuário ou senha inválidos.")
+
+    return render(request, 'login.html')
