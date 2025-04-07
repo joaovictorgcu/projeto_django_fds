60527a6673fea45806ff507759142ce1c8cb4769
João Victor Uchôa <jvgcu@cesar.school>
2025-04-07 09:20:27 -0300

    Tentando mais uma vez

--- Projeto_FDS/asgi.py
+++ Projeto_FDS/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Projeto_FDS.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MATCHAUTOS.settings')
 
 application = get_asgi_application()
--- Projeto_FDS/wsgi.py
+++ Projeto_FDS/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Projeto_FDS.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MATCHAUTOS.settings')
 
 application = get_wsgi_application()
