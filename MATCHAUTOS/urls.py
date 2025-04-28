7b097d2ab89d276607ed66cc125d49deac35bf86
João Victor Uchôa <jvgcu@cesar.school>
2025-04-28 19:22:32 -0300

    .

--- MATCHAUTOS/urls.py
+++ MATCHAUTOS/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('meus-anuncios/', meus_anuncios, name='meus_anuncios'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
